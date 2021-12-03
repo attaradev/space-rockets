7be322412b1a1bd01e6a8771bb352ed1b951fa8f
Mike Attara <mpyebattara@gmail.com>
2021-12-03 03:08:52 +0000

    use ChakraProvider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./components/app";
+import { FavouritesProvider } from "./contexts/favourites";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider>
-        <CSSReset />
-        <App />
-      </ThemeProvider>
+      <ChakraProvider>
+        <FavouritesProvider>
+          <App />
+        </FavouritesProvider>
+      </ChakraProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
